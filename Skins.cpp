3698f73eef7c2f298705d36e162cde4c5fce0fb4
Bram Buurlage <brambuurlage@gmail.com>
2016-05-18 17:21:48 +0200

    more anim spec

--- Skins.cpp
+++ Skins.cpp
@@ -23,4 +23,37 @@ namespace happy
 	{
 		return m_pBindPose.Get();
 	}
+
+	void Animation::setAnimation(const RenderingContext *pRenderContext, const vector<Mat4> &animation, const unsigned bones, const unsigned frames, const float framerate)
+	{
+		m_FrameRate = framerate;
+		m_Looping   = true;
+		m_pFrames   = make_shared<vector<ComPtr<ID3D11Buffer>>>();
+
+		D3D11_BUFFER_DESC poseDesc;
+		ZeroMemory(&poseDesc, sizeof(poseDesc));
+
+		poseDesc.ByteWidth = (UINT)bones * sizeof(Mat4);
+		poseDesc.BindFlags = D3D11_BIND_VERTEX_BUFFER;
+		poseDesc.Usage = D3D11_USAGE_IMMUTABLE;
+
+		auto &bufs = *m_pFrames.get();
+		bufs.reserve(frames);
+		for (unsigned i = 0; i < frames; ++i)
+		{
+			D3D11_SUBRESOURCE_DATA poseData;
+			ZeroMemory(&poseData, sizeof(poseData));
+			poseData.pSysMem = (void*)&animation[i * bones];
+
+			ComPtr<ID3D11Buffer> pose;
+			THROW_ON_FAIL(pRenderContext->getDevice()->CreateBuffer(&poseDesc, &poseData, pose.GetAddressOf()));
+
+			bufs.push_back(pose);
+		}
+	}
+
+	void Animation::setLooping(bool looping)
+	{
+		m_Looping = looping;
+	}
 }
--- Skins.h
+++ Skins.h
@@ -20,6 +20,17 @@ namespace happy
 	class Animation
 	{
 	public:
+		void setAnimation(const RenderingContext *pRenderContext, const vector<Mat4> &animation, const unsigned bones, const unsigned frames, const float framerate);
+		void setLooping(bool looping);
+
+		ID3D11Buffer* getFrame0(float time);
+		ID3D11Buffer* getFrame1(float time);
+		float         getFrameBlend(float time);
+
+	private:
+		float m_FrameRate;
+		bool  m_Looping;
+
 		shared_ptr<vector<ComPtr<ID3D11Buffer>>> m_pFrames;
 	};
 
